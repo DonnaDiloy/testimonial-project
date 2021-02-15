847ef0bed1f30fd89ee6a22814c25b70d50c42b6
DonnaDiloy <74063432+DonnaDiloy@users.noreply.github.com>
2021-02-16 00:11:16 +0800

    initial

---
+++ assets/js/testimonial-project.js
@@ -0,0 +1,9 @@
+(function(){
+    const customerImage = document.querySelector('#customer-image')
+    const customerName = document.querySelector('customer-name')
+    const customerText = document.querySelector('#customer-text')
+    const buttons = document.querySelectorAll('.btn')
+    let index = 0
+    const customer = []
+    
+}
